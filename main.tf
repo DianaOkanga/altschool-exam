fb36ad88615b66ce152031f68381f117e185449b
DianaOkanga <okangadiana@gmail.com>
2024-08-14 12:27:09 +0000

    deleted Terraform directory

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.62.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "terraform-diana-isioma888"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "DianaIsioma"
+    encrypt        = true
+  }
+
+}
+
+provider "aws" {
+  region = var.region
+} 
